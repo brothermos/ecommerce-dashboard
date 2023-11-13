9e3fd573822799177d1e5897042bafddfa9953a8
brothermos <moss10612@gmail.com>
2023-11-13 11:31:23 +0700

    fix(dashboard): edit responsive

--- src/sections/report/report-table.jsx
+++ src/sections/report/report-table.jsx
@@ -13,11 +13,13 @@ import {
   TableHead,
   Container,
   CardHeader,
+  Typography,
 } from '@mui/material';
 
 import { orders } from 'src/_mock/orders';
 
 import Label from 'src/components/label';
+import Scrollbar from 'src/components/scrollbar';
 
 const statusMap = {
   pending: 'warning',
@@ -38,41 +40,45 @@ export const ReportTable = () => (
     >
       <CardHeader title="Latest Orders" />
       <Box sx={{ minWidth: 800 }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ลูกค้า</TableCell>
-              <TableCell>สินค้า</TableCell>
-              <TableCell>จำนวน</TableCell>
-              <TableCell>ราคา</TableCell>
-              <TableCell>Order ID</TableCell>
-              <TableCell sortDirection="desc">วันที่ซื้อ</TableCell>
-              <TableCell>สถานะ</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {orders.map((order) => {
-              const createdAt = format(order.createdAt, 'dd/MM/yyyy');
+        <Scrollbar>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ลูกค้า</TableCell>
+                <TableCell>สินค้า</TableCell>
+                <TableCell>จำนวน</TableCell>
+                <TableCell>ราคา</TableCell>
+                <TableCell>Order ID</TableCell>
+                <TableCell sortDirection="desc">วันที่ซื้อ</TableCell>
+                <TableCell>สถานะ</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {orders.map((order) => {
+                const createdAt = format(order.createdAt, 'dd/MM/yyyy');
 
-              return (
-                <TableRow hover key={order.id}>
-                  <TableCell>{order.customerName}</TableCell>
-                  <TableCell style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-                    <Avatar src={order.cover} />
-                    {order.productName}
-                  </TableCell>
-                  <TableCell>{order.quantity}</TableCell>
-                  <TableCell>{order.price}</TableCell>
-                  <TableCell>{order.orderId}</TableCell>
-                  <TableCell>{createdAt}</TableCell>
-                  <TableCell>
-                    <Label color={statusMap[order.status]}>{order.status}</Label>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+                return (
+                  <TableRow hover key={order.id}>
+                    <TableCell>{order.customerName}</TableCell>
+                    <TableCell component="th" scope="row" padding="none">
+                      <Stack direction="row" alignItems="center" spacing={2}>
+                        <Avatar src={order.cover} />
+                        <Typography noWrap>{order.productName}</Typography>
+                      </Stack>
+                    </TableCell>
+                    <TableCell>{order.quantity}</TableCell>
+                    <TableCell>{order.price}</TableCell>
+                    <TableCell>{order.orderId}</TableCell>
+                    <TableCell>{createdAt}</TableCell>
+                    <TableCell>
+                      <Label color={statusMap[order.status]}>{order.status}</Label>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Scrollbar>
       </Box>
       <Divider />
     </Card>
